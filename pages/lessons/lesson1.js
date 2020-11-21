ac8e4bcccd1be03947a7fcceaa8bf869f25bd423
Nico Carballal <nico@code42.ai>
2020-11-20 23:16:54 -0800

    Clean up lesson page format

--- pages/lessons/lesson1.js
+++ pages/lessons/lesson1.js
@@ -7,21 +7,6 @@ import styles from '../../styles/lesson.module.css'
 
 
 
-const updateScore = e => {
-
-    variable = parseInt(document.querySelector('input[name = "variable"]:checked').value);
-    sub = parseInt(document.querySelector('input[name = "sub"]:checked').value);
-    con = parseInt(document.querySelector('input[name = "con"]:checked').value);
-    ifstate = parseInt(document.querySelector('input[name = "ifstate"]:checked').value);
-    
-    result = variable + sub + con + ifstate;
-    
-    document.getElementById("grade").innerHTML = result;
-         
-    return false;
-}
-
-
 function lesson1() {
     const questions = [
 		{
