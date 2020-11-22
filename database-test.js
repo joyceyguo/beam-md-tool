4d0ed9eabba68d8fc34cb351c88e3ac63a918e77
Darren Omori <darren.omori@berkeley.edu>
2020-11-21 18:32:25 -0800

    migrations to new schema

--- database-test.js
+++ database-test.js
@@ -17,6 +17,36 @@ import { open } from 'sqlite'
         force: true
     })
 
-    const people = await db.all('SELECT * from Category');
+    const result = await db.run(
+    'INSERT INTO Posts (id, body, user_id, reply_id) VALUES ($a, $b, $c, $d)',
+    {$a: 1, $b: "This is test post 1 - parent.", $c: 1, $d: 1}
+    )
+
+    const result2 = await db.run(
+    'INSERT INTO Posts (id, body, user_id, reply_id) VALUES ($a, $b, $c, $d)',
+    {$a: 2, $b: "This is test post 2 - child.", $c: 2, $d: 1}
+    )
+
+    const result3 = await db.run(
+    'INSERT INTO Posts (id, body, user_id, reply_id) VALUES ($a, $b, $c, $d)',
+    {$a: 3, $b: "This is test post 3 - child of child.", $c: 2, $d: 2}
+    )
+
+    const result4 = await db.run(
+    'INSERT INTO Posts (id, body, user_id, reply_id) VALUES ($a, $b, $c, $d)',
+    {$a: 4, $b: "This is test post 4 - parent.", $c: 2, $d: 4}
+    )
+
+    const result5 = await db.run(
+    'INSERT INTO Posts (id, body, user_id, reply_id) VALUES ($a, $b, $c, $d)',
+    {$a: 5, $b: "This is test post 2 - second child.", $c: 2, $d: 1}
+    )
+
+    const people = await db.all('SELECT * from Posts');
+
+    const result6 = await db.run('DELETE FROM Posts')
     console.log("CHILLERS", people)
+
+
+
 })()
